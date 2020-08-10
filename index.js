3ba973e92fab4f043904c34b2784d326061109c3
Kayode Gabriel Abiodun <kayodegabriela@gmail.com>
2020-08-10 16:18:28 +0100

    add functionality

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const http = require('http');
 const requestListener = require('./routes.js');
 
-const host = 'localhost';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(requestListener);
 
-server.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
+server.listen(port, () => {
+  console.log(`Server is running on ${port}`);
 });
